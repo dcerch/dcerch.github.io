622b908fe85c04297cc1754d475e449724c2766b
dcerch <dominiccerchio@gmail.com>
2022-01-09 01:43:57 -0500

    Added Garage to Table app

---
+++ projects/full/Saw to Table/js/modelpicker.js
@@ -0,0 +1,68 @@
+//Create model cards and details when page loads
+window.onload = function() {
+  /*
+  CAROUSEL STEP 2
+  */
+
+  var modelviewer = document.getElementById("modelpicker");
+
+  //Iterate through models and add them as cards
+  Object.entries(modelsobject["models"]).forEach(
+    ([key, value]) => {
+      var cardbuilder = '<div class="col"><div class="card">';
+      cardbuilder += '<img src="' + value["img"] + '" class="card-img-top p-2">';
+      cardbuilder += '<div class="card-body"><h5 class="card-title">' + value["title"] + '</h5>';
+      cardbuilder += '<p class="card-text">' + value["description"] + '</p>';
+      cardbuilder += '<a href="#" class="btn btn-primary modelbutton" data-bs-target="#carouselExampleIndicators" data-bs-slide="next" data-modelid="' + value["id"] + '">Choose this model</a>';
+      cardbuilder += '</div></div></div>';
+
+      //Apply card to DOM
+      modelviewer.innerHTML += cardbuilder;
+    }
+  );
+
+  /*
+  CAROUSEL STEP 3
+  */
+
+  //Model details function
+  var modeldetails = function() {
+    var modeldetails = document.getElementById("modeldetails");
+    var listbuilder = '';
+    var totalprice = 0;
+    var modelkey = this.getAttribute("data-modelid");
+
+    //Build first part
+    listbuilder += '<h1 class="mb-3">' + modelsobject["models"][modelkey]["title"] + ' material and pricing details</h1>';
+    listbuilder += '<div class="list-group">';
+
+    //Iterate through models and add them as cards
+    Object.entries(modelsobject["models"][modelkey]["materials"]).forEach(
+      ([key, value]) => {
+        //Build each material list
+        listbuilder += '<a href="' + value["producturl"];
+        listbuilder += '" target="_blank" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">' + value["productname"];
+        listbuilder += '<span class="badge bg-primary rounded-pill">$' + value["productprice"] + '</span>';
+        listbuilder += '</a>';
+
+        //Parse and add model total
+        totalprice = parseFloat(totalprice) + parseFloat(value["productprice"]);
+      }
+    );
+
+    //Build last part
+    listbuilder += '</div>';
+    listbuilder += '<h1 class="mt-4 display-6">Model total: $' + totalprice + ' dollars</h1>';
+
+    console.log(listbuilder);
+
+    //Apply card to DOM
+    modeldetails.innerHTML += listbuilder;
+  };
+
+  //Get HTMLCollection of model buttons
+  var modelbuttons = document.getElementsByClassName("modelbutton");
+
+  //Listen for model button to be click then call details function
+  Array.from(modelbuttons).forEach(function(e) { e.addEventListener('click', modeldetails); });
+};
---
+++ projects/full/Saw to Table/js/models.js
@@ -0,0 +1,172 @@
+var modelsobject = {
+  "models": [
+    {
+      "id": "0",
+      "img": "img/icons/favicon_io_chair/android-chrome-192x192.png",
+      "title": "Chair",
+      "description": "Very luxurious chair. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+    {
+      "id": "1",
+      "img": "img/models/favicon_io_console/android-chrome-192x192.png",
+      "title": "TV Console",
+      "description": "Very luxurious tv console. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "img": "img/models/favicon_io_closet/android-chrome-192x192.png",
+      "title": "Closet Shelves",
+      "description": "Very luxurious closet shelves. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "img": "img/models/favicon_io_bed/android-chrome-192x192.png",
+      "title": "Bed Frame",
+      "description": "Very luxurious bed frame. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "img": "img/models/favicon_io_mailbox/android-chrome-192x192.png",
+      "title": "Mailbox",
+      "description": "Very luxurious mailbox. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "img": "img/models/favicon_io_ladder/android-chrome-192x192.png",
+      "title": "Ladder",
+      "description": "Very luxurious ladder. Simple but elegant.",
+      "materials": [
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "2x4 Prime Whitewood",
+          "productprice": "6.48",
+          "producturl": "https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        },
+        {
+          "productname": "Polished Chrome Handle Bar",
+          "productprice": "16.63",
+          "producturl": "https://www.homedepot.com/p/GlideRite-2-1-2-in-Center-to-Center-Polished-Chrome-Finish-Solid-Handle-Bar-Pulls-10-Pack-5007-64-PC-10/302470072"
+        }
+      ]
+    },
+  ]
+}
